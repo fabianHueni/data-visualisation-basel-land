ab58d289069b88ee96bc3f5b0a66b90a44bd3235
Fabian Hüni <fabian@scaly.ch>
2023-04-19 15:04:29 +0200

    added primeicons and added initial navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import {Component} from '@angular/core';
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  items: MenuItem[];
+
+  activeItem: MenuItem;
+
+  constructor() {
+    this.items = [
+      {label: 'Visualisierung', icon: 'pi pi-fw pi-chart-bar'},
+      {label: 'Daten', icon: 'pi pi-fw pi-database'},
+      {label: 'About', icon: 'pi pi-fw pi-info-circle'},
+      {label: 'Dokumentation', icon: 'pi pi-fw pi-file'},
+    ];
+
+    this.activeItem = this.items[0] ?? null;
+  }
+
+  onActiveItemChange(event: MenuItem){
+    this.activeItem = event;
+  }
+}
