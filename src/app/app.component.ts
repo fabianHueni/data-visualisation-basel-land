0e244f3721f9ea324ae69bf2ffd53cf53af02c7a
Fabian Hüni <fabian@scaly.ch>
2023-05-30 22:16:08 +0200

    removed unused call for median data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,8 @@ import { PopulationService } from './common/service/population.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'data-visualisation-basel-land';
 
-  constructor(private dataService: PopulationService) {}
-
-  async ngOnInit() {
-    const personsByCity =
-      this.dataService.getAgeMedianPerMunicipalityByYear(2022);
-    /*
-    console.log(personsByCity);
-    console.log(personsByCity.get('Wintersingen'));*/
-  }
+  constructor() {}
 }
